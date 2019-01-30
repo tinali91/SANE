07fcc1b16d5e0c1a88808cb4847641a4a0d0f9cf
Ginger KRC <gingerkr@gmail.com>
2019-01-30 09:56:04 -0700

    css footer changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync(
-  // {force: true}
+  {force: true}
   ).then(function() {
     app.listen(PORT, function() {
       console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
